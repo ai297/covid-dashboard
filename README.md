6ff4dc4d73cba08a9c002cd04d4ab43e88b00e7c
Kris Casper <sw.darkwind@gmail.com>
2020-12-13 16:51:00 +0300

    init: configure webpack

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Covid dashboard
-### Task:
+## Task:
 * https://github.com/rolling-scopes-school/tasks/blob/master/tasks/covid-dashboard.md
 
-### Default Covid API from task:
+## Default Covid API from task:
 * https://api.covid19api.com
 * https://api.covid19api.com/summary
 
-### Our UI prototype in Figma:
+## UI prototype in Figma:
 * https://www.figma.com/file/13Amx4uSbMwJCzFFm1q9Ix/COVID-19-DASHBOARD?node-id=0%3A1
 
 
-### Useful links:
+## Useful links:
 * Web-components - https://learn.javascript.ru/custom-elements
 * Getters & Setters - https://learn.javascript.ru/property-accessors
+
+## Project features:
+* **npm run dev** - command to lounch webpack dev server
+* **npm run build** - command to build application
+* **/public** - base folder for all static files in project
+* **/public/assets** - base folder for images, icons, fonts, etc...
+* **/src** - work folder for all scripts and styles
+* **/dist** - build path
+* **index.js** - entry point
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require('webpack');
+
+const APPLICATION_FILES_PATH = path.resolve(__dirname, 'src');
+const STATIC_FILES_PATH = path.resolve(__dirname, 'public');
+const BUILD_PATH = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  context: APPLICATION_FILES_PATH,
+  //entry: ["@babel/polyfill", './index.js'],
+  entry: ['./index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_PATH,
+  },
+  devtool: 'eval',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: STATIC_FILES_PATH,
+          to: BUILD_PATH,
+        },
+      ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ESLintPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          }
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(ico|png|jpe?g|gif|svg|mp3|wav|ttf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: STATIC_FILES_PATH,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  }
+};

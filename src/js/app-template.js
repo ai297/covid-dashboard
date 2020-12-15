6f3222eb626da7c3f51c211ef331144de3e59844
Kris Casper <sw.darkwind@gmail.com>
2020-12-16 01:36:31 +0300

    feat: continue implement styles. add footer style, github links, rs-school logo, scrollable section

--- src/js/app-template.js
+++ src/js/app-template.js
@@ -17,10 +17,14 @@ const getTemplate = () => `
     <section class="dashboard__section dashboard__header"><h1 class="widget-title">Covid-2019 dashboard</h1></section>
     <section class="dashboard__section dashboard__section--grow">
       <search-field class="search-form">Placeholder?</search-field>
-      <countries-list class="countries-list">
-        <countries-list-item class="countries-list__item" flag="./flag icon url" name="USA">16135597</countries-list-item>
-        <countries-list-item class="countries-list__item" flag="./flag icon url" name="Russia">2629699</countries-list-item>
-      </countries-list>
+      <div class="widget-container widget-container--grow">
+        <div class="widget-container__scroll custom-scrollbar">
+          <countries-list class="countries-list">
+            <countries-list-item class="countries-list__item" flag="./flag icon url" name="USA"><span class="cases">16135597</span></countries-list-item>
+            <countries-list-item class="countries-list__item" flag="./flag icon url" name="Russia"><span class="cases">2629699</span></countries-list-item>
+          </countries-list>
+        </div>
+      </div>
       <tabs-group class="tabs-group">
         <tabs-button class="tabs-group__button" selected="true">Cases</tabs-button>
         <tabs-button class="tabs-group__button">Recovered</tabs-button>
@@ -31,13 +35,13 @@ const getTemplate = () => `
         <toggle-button>Total / p 100k</toggle-button>
       </toggle-group>
     </section>
-    <section class="dashboard__section">
+    <section class="dashboard__section last-update">
       <p>Last update: 14.12.2020 12:00</p>
     </section>
   </div>
   <div class="dashboard__column dashboard__column--map-column">
-    <section class="map-section"></section>
-    <section class="map-controls">
+    <section class="map-section">Map should be here</section>
+    <section class="dashboard__section map-controls">
       <tabs-group class="tabs-group">
         <tabs-button class="tabs-group__button" selected="true">Cases</tabs-button>
         <tabs-button class="tabs-group__button">Recovered</tabs-button>
@@ -52,7 +56,7 @@ const getTemplate = () => `
   <div class="dashboard__column dashboard__column--widget-column">
     <section class="dashboard__section">
       <h2 class="widget-title">Worldwide</h2>
-      <div class="table-container">
+      <div class="widget-container">
         <table class="widget-table">
           <tr>
             <td>Cases</td>
@@ -74,7 +78,9 @@ const getTemplate = () => `
       </toggle-group>
     </section>
     <section class="dashboard__section">
-      <app-chart class="chart"></app-chart>
+      <div class="widget-container">
+        <app-chart class="chart">Chart will be here</app-chart>
+      </div>
       <toggle-group class="toggle-group">
         <toggle-button class="toggle-group__button" active="true">Today / All time</toggle-button>
         <toggle-button>Total / p 100k</toggle-button>
